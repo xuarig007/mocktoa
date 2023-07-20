3b521f2faaf6a781a954afa0731b86afaf7416bf
XUARIG <35503724+xuarig007@users.noreply.github.com>
2023-07-20 17:58:50 +0200

    Update simple_usage.py

--- simple_usage.py
+++ simple_usage.py
@@ -1,17 +1,18 @@
 import mocktoa
 
-# mocktoa instance 
+print("1) New instance of Mocktoa => mt = mocktoa.Mocktoa()\n")
+# mocktoa instance
 mt = mocktoa.Mocktoa()
 
 # types list available
-print(f"Available types : { mt.get_keys() }")
+print(f"2) Available types => list(mt.get_keys()) => { list(mt.get_keys()) }\n")
 
 # random type data
 type_data = mt.get_random_type()
-print(f"type_data = {type_data}")
+print(f"3) Random type_data => type_data = mt.get_random_type() => \"{type_data}\"\n")
 
 # get random data for type data and operating system type
 typeOs="Linux Server"
 data=mt.get_data_from_type(type_data,typeOs)
 
-print("Data = { data }") 
+print(f"4) Get data for {typeOs} =>  data=mt.get_data_from_type(type_data,typeOs) => { data }\n") 
